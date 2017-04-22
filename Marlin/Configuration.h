903c277bf26bbb3fd9cae6f2e9c8108ba8ab3497
Alexander Larsson <alexl@redhat.com>
2017-04-23 00:54:02 +0200

    Calibrate curvature

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -438,7 +438,7 @@
   #define DELTA_DIAGONAL_ROD 215.0 // mm
 
   // Horizontal offset from middle of printer to smooth rod center.
-  #define DELTA_SMOOTH_ROD_OFFSET 145.0 // mm
+  #define DELTA_SMOOTH_ROD_OFFSET 145.2 // mm
 
   // Horizontal offset of the universal joints on the end effector.
   #define DELTA_EFFECTOR_OFFSET 19.9 // mm
